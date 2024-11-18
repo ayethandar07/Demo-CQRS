6ab3a4f0db1421677c869cc38a25116152a74da7
aye <ayethandarwin@innovix-solutions.com>
2024-11-18 15:37:20 +0630

    add create command handler

---
+++ CleanArchitecture.Application/Blogs/Commands/CreateBlog/CreateBlogCommand.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Application.Blogs.Queries.GetBlogs;
+using MediatR;
+
+namespace CleanArchitecture.Application.Blogs.Commands.CreateBlog;
+
+public class CreateBlogCommand: IRequest<BlogVm>
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Author { get; set; }
+}
---
+++ CleanArchitecture.Application/Blogs/Commands/CreateBlog/CreateBlogCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Blogs.Queries.GetBlogs;
+using CleanArchitecture.Domain.Entity;
+using CleanArchitecture.Domain.Repositoty;
+using MediatR;
+
+namespace CleanArchitecture.Application.Blogs.Commands.CreateBlog;
+
+public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, BlogVm>
+{
+    private readonly IBlogRepository _blogRepository;
+    private readonly IMapper _mapper;
+
+    public CreateBlogCommandHandler(IBlogRepository blogRepository, IMapper mapper)
+    {
+        _blogRepository = blogRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<BlogVm> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+    {
+        var blogEntity = new Blog() { Name = request.Name, Author = request.Author, Description = request.Description };
+
+        var res = await _blogRepository.CreateBlogAsync(blogEntity);
+        return _mapper.Map<BlogVm>(res);
+    }
+}
