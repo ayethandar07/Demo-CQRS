50de5ef6a8d0e730502b37c5e0be8afb86c3b945
aye <ayethandarwin@innovix-solutions.com>
2024-11-18 17:15:19 +0630

    add update

--- CleanArchitecture.API/Controllers/BlogController.cs
+++ CleanArchitecture.API/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
-﻿using CleanArchitecture.Application.Blogs.Queries.GetBlogById;
+﻿using CleanArchitecture.Application.Blogs.Commands.CreateBlog;
+using CleanArchitecture.Application.Blogs.Commands.UpdateBlog;
+using CleanArchitecture.Application.Blogs.Queries.GetBlogById;
 using CleanArchitecture.Application.Blogs.Queries.GetBlogs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,14 +12,14 @@ namespace CleanArchitecture.API.Controllers;
 public class BlogController : ApiControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetAllAsync()
+    public async Task<IActionResult> GetAll()
     {
         var blogs = await Mediator.Send(new GetBlogQuery());
         return Ok(blogs);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetByIdAsync(int id)
+    public async Task<IActionResult> GetById(int id)
     {
         var blog = await Mediator.Send(new GetBlogByIdQuery() { BlogId = id});
         if(blog == null)
@@ -25,5 +27,24 @@ public class BlogController : ApiControllerBase
             return NotFound();
         }
         return Ok(blog);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateBlogCommand command)
+    {
+        var createdBlog = await Mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id = createdBlog.Id }, createdBlog);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(int id, UpdateBlogCommand command)
+    {
+        if( id != command.Id )
+        {
+            return BadRequest();
+        }
+
+        await Mediator.Send(command);
+        return NoContent();
     }
 }
